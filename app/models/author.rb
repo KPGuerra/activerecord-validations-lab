1cd0fd06980d8dae69749b6476baf4844cc1b068
KPGuerra <KPGuerra96@gmail.com>
2020-10-29 23:09:11 -0400

    Passed

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ActiveRecord::Base
+    validates  :name, presence: true, uniqueness: true
+    validate :phone_number_too_short
+
+    def phone_number_too_short
+        if phone_number.length < 10
+            errors.add(:phone_number, "Phone Number too short")
+        end
+    end
+
 end
